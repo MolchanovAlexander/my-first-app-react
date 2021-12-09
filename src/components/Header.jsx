e01cafccc9f2d547f45dcf9d792dca90a48649d0
Alexander Molchanov <rybkolhoz@gmail.com>
2021-12-09 16:46:04 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRka9KWpogyMvNRaflcGnjpmXeXgV4lAh6_7A&usqp=CAU" />
+    </header>
+  );
+};
+export default Header;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="nav">
+      <div>
+        <a>Profile</a>
+      </div>
+      <div>
+        <a>Messeges</a>
+      </div>
+      <div>
+        <a>News guf</a>
+      </div>
+      <div>
+        <a>Musik</a>
+      </div>
+    </nav>
+  );
+};
+export default NavBar;
