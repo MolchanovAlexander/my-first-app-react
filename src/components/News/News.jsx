52864911743a1b82d86066a58b549b29f8496026
Olexander <rybkolhoz@gmail.com>
2021-12-20 20:57:44 +0200

    img add

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import s from "./News.module.css";
+import img2 from "./src/pic/2.jpg"
 
 const News = (props) => {
   
   return (
-   <div className={s.newsItems}><img src={'src/pic/2.jpg'} alt="123" />
+   <div className={s.newsItems}><img src={img2} alt="123" />
      My NEWS
       <div ></div>
       <div >
