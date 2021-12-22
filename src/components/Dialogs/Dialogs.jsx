67752c5127afb1a0a186b6102ac4ebc71cd3ec86
Olexander <rybkolhoz@gmail.com>
2021-12-22 14:23:16 +0200

    formated

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,27 +4,27 @@ import { NavLink } from "react-router-dom";
 
 
 const Dialogs = (props) => {
-  
+
   return (
-   <div >
-     <div className={s.dialogs}>
-       <div className={s.dialogsItem}>
-         <div className={s.dialog+' '+s.active}> <NavLink to="/dialogs/1">Gufiy</NavLink></div>
-         <div className={s.dialog}><NavLink to="/dialogs/1">Sveta</NavLink></div>
-         <div className={s.dialog}><NavLink to="/dialogs/2">Nykyhvir</NavLink></div>
-         <div className={s.dialog}><NavLink to="/dialogs/3">Dniwe</NavLink></div>
-         <div className={s.dialog}><NavLink to="/dialogs/4">Ilona</NavLink></div>
-         <div className={s.dialog}><NavLink to="/dialogs/5">Alesya</NavLink></div>
-         <div className={s.dialog}><NavLink to="/dialogs/6">Katya</NavLink></div>
-       </div>
-     
-     <div className={s.messages}>
-       <div className={s.message}>What guf is in your hom</div>
-       <div className={s.message}>yo noob</div>
-       <div className={s.message}>dniwe!!!!</div>
-       <div className={s.message}>Gufiy</div>
-     </div>
-     </div>
+    <div >
+      <div className={ s.dialogs }>
+        <div className={ s.dialogsItem }>
+          <div className={ s.dialog + ' ' + s.active }> <NavLink to="/dialogs/1">Gufiy</NavLink></div>
+          <div className={ s.dialog }><NavLink to="/dialogs/1">Sveta</NavLink></div>
+          <div className={ s.dialog }><NavLink to="/dialogs/2">Nykyhvir</NavLink></div>
+          <div className={ s.dialog }><NavLink to="/dialogs/3">Dniwe</NavLink></div>
+          <div className={ s.dialog }><NavLink to="/dialogs/4">Ilona</NavLink></div>
+          <div className={ s.dialog }><NavLink to="/dialogs/5">Alesya</NavLink></div>
+          <div className={ s.dialog }><NavLink to="/dialogs/6">Katya</NavLink></div>
+        </div>
+
+        <div className={ s.messages }>
+          <div className={ s.message }>What guf is in your hom</div>
+          <div className={ s.message }>yo noob</div>
+          <div className={ s.message }>dniwe!!!!</div>
+          <div className={ s.message }>Gufiy</div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,21 +4,21 @@ import s from "./NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <nav className={s.nav}>
-      <div className={`${s.item} `}>
-        <NavLink to='/profile' className = {navData => navData.isActive ? s.active : s.item }>Profile</NavLink>
+    <nav className={ s.nav }>
+      <div className={ `${s.item} ` }>
+        <NavLink to='/profile' className={ navData => navData.isActive ? s.active : s.item }>Profile</NavLink>
       </div>
-      <div className={s.item}>
-        <NavLink to='/dialogs' className ={navData => navData.isActive ? s.active : s.item }>Messeges</NavLink>
+      <div className={ s.item }>
+        <NavLink to='/dialogs' className={ navData => navData.isActive ? s.active : s.item }>Messeges</NavLink>
       </div>
-      <div className={s.item}>
-        <NavLink to="/news" className ={navData => navData.isActive ? s.active : s.item }>News</NavLink>
+      <div className={ s.item }>
+        <NavLink to="/news" className={ navData => navData.isActive ? s.active : s.item }>News</NavLink>
       </div>
-      <div className={s.item}>
-        <NavLink to="/music" className ={navData => navData.isActive ? s.active : s.item }>Musik</NavLink>
+      <div className={ s.item }>
+        <NavLink to="/music" className={ navData => navData.isActive ? s.active : s.item }>Musik</NavLink>
       </div>
-      <div className={s.item}>
-        <NavLink to="/settings" className ={navData => navData.isActive ? s.active : s.item }>Settings</NavLink>
+      <div className={ s.item }>
+        <NavLink to="/settings" className={ navData => navData.isActive ? s.active : s.item }>Settings</NavLink>
       </div>
     </nav>
   );
